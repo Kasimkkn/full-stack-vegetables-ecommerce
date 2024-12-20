74db5ee5a40bf8c87ae5c22b1df25d9bf62451b7
Kasim-bigshorts <Kasim.Kadiwala@bigshorts.co>
2024-12-20 17:56:55 +0530

    fixed dependicy error

--- vegies-backend/src/controllers/product.ts
+++ vegies-backend/src/controllers/product.ts
@@ -178,29 +178,13 @@ export const deleteProduct = TryCatch(async (req, res, next) => {
 });
 export const getAllProducts = TryCatch(async (req: Request<{}, {}, {}, SearchRequestQuery>, res, next) => {
 
-  const { search, sort, category, price, } = req.query;
-
   const page = Number(req.query.page) || 1;
   const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
   const skip = (page - 1) * limit;
 
   const baseQuery: BaseQuery = {};
 
-  if (search) {
-    baseQuery.name = {
-      $regex: search,
-      $options: "i",
-    };
-  }
-  if (price) {
-    baseQuery.price = {
-      $lte: Number(price),
-    };
-  }
-
-  if (category) baseQuery.category = category;
-
-  const productsPromise = Product.find(baseQuery).sort(sort && { price: sort === "asc" ? 1 : -1 }).limit(limit).skip(skip);
+  const productsPromise = Product.find(baseQuery).limit(limit).skip(skip);
 
   const [products, filteredOnlyProduct] = await Promise.all([
     productsPromise,
--- vegies-backend/src/types/types.ts
+++ vegies-backend/src/types/types.ts
@@ -51,10 +51,6 @@ export type ControllerType = (
 ) => Promise<void | Response<any, Record<string, any>>>;
 
 export type SearchRequestQuery = {
-  search?: string;
-  price?: string;
-  category?: string;
-  sort?: string;
   page?: string;
 };
 
